a06af92c866da00f5dd5632c1b21bfb181e80f0d
Clarks1223 <pablo.uchuari@epn.edu.ec>
2024-05-08 00:17:11 -0500

    cors configurados para que se pueda escuchar la app desde cualquier dominio

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import routerMetro from './routes/rutasMetro.routes.js';
+import cors from "cors";
+
 dotenv.config();
 const app = express();
 //configuracion del puerto
 //Configuraciones
 app.set('port', process.env.PORT || 3000);
+//permite que las solicitudes se hagan desde otros dominios
+app.use(cors());
 //middlewares
 app.use(express.json());
 //importo las rutas
