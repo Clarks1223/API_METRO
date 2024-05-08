2ee53321db20aa20019fa4470a55268fcc773c63
Clarks1223 <pablo.uchuari@epn.edu.ec>
2024-05-07 23:10:50 -0500

    preparacion para el puerto, agregado en variable de entorno

--- src/server.js
+++ src/server.js
@@ -3,9 +3,13 @@ import dotenv from 'dotenv';
 import routerMetro from './routes/rutasMetro.routes.js';
 dotenv.config();
 const app = express();
+//configuracion del puerto
+//Configuraciones
+app.set('port', process.env.PORT || 3000);
 //middlewares
 app.use(express.json());
 //importo las rutas
+app.use('/', (req, res) => res.status(200).send('API del metro de Quito, ruta raiz!'));
 app.use('/api', routerMetro);
 //en caso de que no encuentre las rutas
 app.use((req, res) => {
